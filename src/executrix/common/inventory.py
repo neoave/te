206c4e2a3c2d4bddc8ef8c2539f30edced5855d3
Petr Vobornik <pvoborni@redhat.com>
2022-10-08 22:14:27 +0200

    fix: backport host finding fixes
    - to be able to work with mrack generated inventories
    - to not crash if hosts or host doesn't exists
    
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

--- src/executrix/common/inventory.py
+++ src/executrix/common/inventory.py
@@ -12,12 +12,20 @@ def to_external_hostname(hostname):
     """Get connectable hostname or IP address from job hostname."""
     inv_path = os.path.join(test_dir(), INVENTORY)
     inventory = read_yaml(inv_path)
+    ansible_host = None
 
+    # First try if host is defined in 'all' group
+    hosts = inventory["all"].get("hosts", {})
+    host = hosts.get(hostname, {})
+    ansible_host = host.get("ansible_host")
+    if ansible_host:
+        return ansible_host
+
+    # If not, try to find it in other groups
     for group in inventory["all"]["children"].items():
-        hosts = group[1].get("hosts")
-        if not hosts:
-            continue
-        host = hosts.get(hostname)
-        if host:
-            return host.get("ansible_host")
-    return None
+        hosts = group[1].get("hosts", {})
+        host = hosts.get(hostname, {})
+        ansible_host = host.get("ansible_host")
+        if ansible_host:
+            break
+    return ansible_host
