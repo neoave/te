25a8530495aed7ca608f84a1dc888ec9daa0c693
Petr Vobornik <pvoborni@gmail.com>
2024-05-18 18:59:21 +0200

    test: fix test_smoke
    The basic test was failing.
    
    Signed-off-by: Petr Vobornik <pvoborni@gmail.com>

--- tests/unit/test_steps.py
+++ tests/unit/test_steps.py
@@ -3,4 +3,4 @@ from te.common.step import StepTypes, step_types
 
 def test_smoke():
     """Smoke test of Step registration."""
-    assert type(step_types) == StepTypes
+    assert isinstance(step_types, StepTypes)
